d9baba5f55bb6725739095dd1b345410af8f8408
key55 <15725571+key55@users.noreply.github.com>
2024-07-13 10:55:15 +0200

    NameError 'model_to_args'

--- src/baseline_deep_models/baseline.py
+++ src/baseline_deep_models/baseline.py
@@ -10,55 +10,32 @@ filename = os.path.dirname(__file__)[:-1]
 filename = "/".join(filename.split("/")[:-1])
 sys.path.append(os.path.join(filename, 'preprocess'))
 
-from tokenizer import Tokenizer
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tokenizer import Tokenizer as CustomTokenizer
 from torchtext.vocab import GloVe
 from sklearn.model_selection import StratifiedKFold
+from keras.models import Sequential
+from keras.layers import Dense, Bidirectional, LSTM, Dropout, BatchNormalization, Embedding
+from tensorflow.keras.models import save_model
+from tensorflow.keras.models import load_model
 from sklearn.metrics import accuracy_score
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from collections import Counter
-from torch.utils.data import TensorDataset, DataLoader
-import time
-from tqdm import tqdm
 
-if torch.cuda.is_available():
-    print("GPU is available")
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    print(f"Num GPUs Available: {len(gpus)}")
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
 else:
-    print("GPU is NOT available")
+    print("No GPUs Available")
 
-device = torch.device("gpu") if torch.cuda.is_available() else torch.device("cpu")
-
-class BiLSTM(nn.Module):
-    def __init__(self, embed_dim, drop_prob):
-        super().__init__()
-        self.hidden_size = 100
-        self.input_size = embed_dim
-        self.num_layers = 1
-        self.bidirectional = True
-        self.num_directions = 1
-        self.dropout1 = nn.Dropout(p=drop_prob)
-
-        if self.bidirectional:
-            self.num_directions = 2
-
-        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, bidirectional=self.bidirectional)
-        self.fc = nn.Linear(self.hidden_size*self.num_directions, 1)
-
-    def forward(self, tweet):
-        lstm_out, _ = self.lstm(tweet.view(len(tweet), 1, -1))
-        x = self.dropout1(lstm_out.view(len(tweet), -1))
-        output = self.fc(x)
-        pred = torch.sigmoid(output[-1])
-        return pred
-
-
-def write_to_log(model_type:str,
+def write_to_log(model_type:str, model_args:dict,
                  log_path:str, log_filename:str,
                  mean_accuracy:float, std_accuracy:str,
                  glove_dim: int,
-                 data_type:List[str],
-                 model_args:dict=None):
+                 data_type:List[str]):
 
     print("Logging the results to the log file")
     log = f"glove {glove_dim} + {model_type}"
@@ -76,170 +53,113 @@ def write_to_log(model_type:str,
         f.write("\n-------------------------------------\n")
     print("Wrote to the log file")
 
-def load_glove_embeddings(glove_path: str):
-    print("Loading GloVe embeddings")
-    embeddings = {}
-    total = 0
-    with open(glove_path, "r") as f:
-        total = len(f.readlines())
+def cross_validation(tweets, labels, model_type:str, glove_dim:int):
+    processed_tweets = []
+    lengths = []
+    custom_tokenizer = CustomTokenizer(reduce_len=True, segment_hashtags=True)
+    for tweet in tweets:
+        tokenized_tweet = custom_tokenizer.tokenize_tweet(tweet=tweet)
+        processed_tweets.append(" ".join(tokenized_tweet))
+        lengths.append(len(tokenized_tweet))
+    lengths = np.array(lengths)
+    processed_tweets = np.array(processed_tweets)
+    max_len = lengths.max()
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(processed_tweets)
+    vocab_size = len(tokenizer.word_index)+1
+    encoded_tweets = tokenizer.texts_to_sequences(processed_tweets)
+    padded_tweets = pad_sequences(encoded_tweets, maxlen=max_len, padding="post")
 
-    with open(glove_path, "r") as f:
-        with tqdm(total=total) as pbar:
-            for line in f:
-                word, weights = line.split(maxsplit=1)
-                weights = np.fromstring(weights, "f", sep=" ")
-                embeddings[word] = weights
-                pbar.update(1)
-    return embeddings
+    embed_matrix = np.zeros((vocab_size, glove_dim))
+    glove = GloVe(name="twitter.27B", dim=glove_dim)
+    for word, i in tokenizer.word_index.items():
+        embedding_vec = glove.get_vecs_by_tokens(word)
+        if embedding_vec is not None:
+            embed_matrix[i] = embedding_vec
 
-def tweet_embed(words, embeddings, glove_dim):
-    unknown_indices = []
-    mean = np.zeros(glove_dim)
-    for i in range(len(words)):
-        if words[i] in embeddings.keys():
-            words[i] = embeddings[words[i]]
-            mean += words[i]
-        else:
-            unknown_indices.append(i)
-    if (len(words) - len(unknown_indices)) != 0:
-        mean /= (len(words) - len(unknown_indices))
-    for i in unknown_indices:
-        words[i] = mean
-    return np.array(words)
-
-def cross_validation(data, model_type:str, glove_dim:int, glove_path:str):
-
-    custom_tokenizer = Tokenizer(reduce_len=True, segment_hashtags=True, post_process=True)
-
-    embeddings = load_glove_embeddings(glove_path)
-
-    print("Transforming tweets")
-
-    data["text"] = data["text"].apply(lambda x: custom_tokenizer.tokenize_tweet(x))
-    data[data["text"].map(len) >= 1]
-    data["text"] = data["text"].apply(lambda x: tweet_embed(x, embeddings, glove_dim))
-
-
-    skf = StratifiedKFold(n_splits=2, shuffle=True, random_state=42)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
     aggregated_acc = []
 
-    tweets = data["text"].values
-    labels = data["labels"].values
-
-    aggregated_acc = []
-
-    for i, (train_indices, test_indices) in enumerate(skf.split(tweets, labels)):
-        training_tweets = tweets[train_indices]
+    for i, (train_indices, test_indices) in enumerate(skf.split(padded_tweets, labels)):
+        training_tweets = padded_tweets[train_indices]
         training_labels = labels[train_indices]
 
-        val_tweets = tweets[test_indices]
+        val_tweets = padded_tweets[test_indices]
         val_labels = labels[test_indices]
 
+        accuracy = None
+        model = None
+
         if model_type == "bilstm":
-            bilstm = BiLSTM(glove_dim, 0.2)
-            loss_func = nn.BCELoss()
-            optimizer = torch.optim.Adam(bilstm.parameters(), lr=0.001)
-            scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1)
-            train_losses = []
-            val_losses = []
-            train_accs = []
-            val_accs = []
-            for epoch in range(4):
-                epoch_start_time = time.time()
-                print("Epoch: {}".format(epoch+1))
-                train_loss = 0
-                correct = 0
-                bilstm.train()
-                for i in range(len(training_tweets)):
-                    bilstm.zero_grad()
-                    tweet = torch.FloatTensor(training_tweets[i])
-                    label = torch.FloatTensor(np.array([training_labels[i]]))
-                    if torch.cuda.is_available():
-                        tweet = tweet.cuda()
-                        label = label.cuda()
-                    pred = bilstm(tweet)
-                    loss = loss_func(pred, label)
-                    lambda_param = torch.tensor(0.001)
-                    l2_reg = torch.tensor(0.)
+            ## Uncomment again
+            # model = Sequential()
+            # model.add(Embedding(vocab_size, glove_dim, input_length=max_len, weights=[embed_matrix], trainable=False))
+            # model.add(Bidirectional(LSTM(20, return_sequences=True)))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+            # model.add(Bidirectional(LSTM(20, return_sequences=True)))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+            # model.add(Bidirectional(LSTM(20)))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+            # model.add(Dense(64, activation="relu"))
+            # model.add(Dense(64, activation="relu"))
+            # model.add(Dense(1, activation="sigmoid"))
+            # model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+            # model.fit(training_tweets, training_labels, epochs=10, batch_size=64)
 
-                    if torch.cuda.is_available():
-                        lambda_param = lambda_param.cuda()
-                        l2_reg = l2_reg.cuda()
-                    for param in bilstm.parameters():
-                        if torch.cuda.is_available():
-                            l2_reg += torch.norm(param).cuda()
-                        else:
-                            l2_reg += torch.norm(param)
-                    loss += lambda_param * l2_reg
 
-                    pred = pred.item()
-                    if pred > 0.5:
-                        pred = 1
-                    else:
-                        pred = 0
-                    if pred == int(label.item()):
-                        correct += 1
-                    train_loss += loss.item()
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
+            #y_preds = model.predict(val_tweets)
+            #accuracy = accuracy_score(val_labels, y_preds)
+            # accuracy = keras.losses.binary_crossentropy(val_labels, y_preds)
+            
+            ## Uncomment again
+            # model_save_dir = "/mnt/galactica/rhyners/CIL/twitter_sentiment_analysis-tunaberk/src/save_models/"
+            # if not os.path.exists(model_save_dir):
+            #     try:
+            #         os.makedirs(model_save_dir)
+            #         print(f"Directory {model_save_dir} created successfully.")
+            #     except Exception as e:
+            #         print(f"Failed to create directory {model_save_dir}: {e}")
+            #         return
 
-                    if (i+1)%1000 == 0:
-                        print("Processed {} tweets out of {}".format(i+1, len(training_tweets)))
-                train_losses.append(train_loss/len(training_tweets))
-                train_accs.append(correct / len(training_tweets))
-
-                val_loss = 0
-                correct = 0
-                bilstm.eval()
-                with torch.no_grad():
-                    for i in range(len(val_tweets)):
-                        tweet = torch.FloatTensor(val_tweets[i])
-                        label = torch.FloatTensor(np.array([val_labels[i]]))
-
-                        if torch.cuda.is_available():
-                            tweet = tweet.cuda()
-                            label = label.cuda()
-                        pred = bilstm(tweet)
-                        loss = loss_func(pred, label)
-                        val_loss += loss.item()
-                        pred = pred.item()
-                        if pred > 0.5:
-                            pred = 1
-                        else:
-                            pred = 0
-                        if pred == int(label.item()):
-                            correct += 1
-                val_losses.append(val_loss/len(val_tweets))
-                val_accs.append(correct/len(val_labels))
-                print("Epoch summary")
-                print(f'Train Loss: {train_losses[-1]:7.2f}  Train Accuracy: {train_accs[-1]*100:6.3f}%')
-                print(f'Validation Loss: {val_losses[-1]:7.2f}  Validation Accuracy: {val_accs[-1]*100:6.3f}%')
-                print(f'Duration: {time.time() - epoch_start_time:.0f} seconds')
-                print('')
-
-                scheduler.step()
-            aggregated_acc.append(val_accs[-1])
-            mean_accuracy = np.array(aggregated_acc).mean()
-            std_accuracy = np.array(aggregated_acc).std()
-            return mean_accuracy, std_accuracy
+            model_save_path = os.path.join(model_save_dir, f"baseline_deep_models_model_fold_{i}.keras")
+            # try:
+            #     save_model(model, model_save_path)
+            #     print(f"Model saved successfully at {model_save_path}")
+            #     print("WARNING: You are saving your model as an HDF5 file via `model.save()` or `keras.saving.save_model(model)`. This file format is considered legacy.")
+            #     print("We recommend using instead the native Keras format, e.g. `model.save('my_model.keras')` or `keras.saving.save_model(model, 'my_model.keras')`.")
+            # except Exception as e:
+            #     print(f"Error saving model at {model_save_path}: {e}")
+            model = load_model(model_save_path)
+            loss_and_metrics = model.evaluate(val_tweets, val_labels)
+            print(f"Loss: {loss_and_metrics[0]}")
+            print(f"Accuracy: {loss_and_metrics[1]}")
+            accuracy = loss_and_metrics[1]
+        aggregated_acc.append(accuracy)
+    print(aggregated_acc)
+    mean_accuracy = np.array(aggregated_acc).mean()
+    std_accuracy = np.array(aggregated_acc).std()
+    print(f"Accuracy: {mean_accuracy}, Std: {std_accuracy}")
+    return mean_accuracy, std_accuracy
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", help="Input path of the preprocessed csv file")
     parser.add_argument("--log_path", help="Path of the log folder")
     parser.add_argument("--log_filename", help="Name of the log file", default="logs")
-    parser.add_argument("--glove_path", help="Path of the Glove embeddings", default="logs")
     args = parser.parse_args()
-    if args.input_path is None or args.log_path is None or args.log_filename is None or args.glove_path is None:
-        print("Input path flag, log path, glove_path or log filename flag cannot be none")
+    if args.input_path is None or args.log_path is None or args.log_filename is None:
+        print("Input path flag, log path or log filename flag cannot be none")
         exit()
     if not os.path.exists(f"{args.log_path}"):
         os.makedirs(f"{args.log_path}")
     preprocessed_data_type = args.input_path.split("/")[-1][:-4].split("_")[1:]
     train_df = pd.read_csv(args.input_path)
-    train_df = train_df.sample(n=100000) # TODO: Comment this line when not testing
     train_df = train_df.dropna()
+    tweets = np.array(train_df["text"].values)
+    labels = np.array(train_df["labels"].values)
     """
     pos_tweets = np.array(train_df[train_df["labels"] == 1]["text"].values)[:1000]
     pos_labels = labels[:1000]
@@ -258,16 +178,18 @@ if __name__ == "__main__":
                                 log_filename=args.log_filename,
                                 data_type=preprocessed_data_type)
     """
-
-    for glove_dim in [100]:
+    for glove_dim in [100, 200]:
         for model_type in ["bilstm"]:
             mean_accuracy = None
             std_accuracy = None
-            mean_accuracy, std_accuracy = cross_validation(data=train_df,
+            mean_accuracy, std_accuracy = cross_validation(tweets=tweets, labels=labels,
                                                            glove_dim=glove_dim,
-                                                            model_type=model_type,
-                                                            glove_path=args.glove_path)
-            write_to_log(glove_dim=glove_dim, model_type=model_type,
+                                                            model_type=model_type)
+            ## Traceback (most recent call last):
+            ## File "/mnt/galactica/rhyners/CIL/twitter_sentiment_analysis-tunaberk/src/baseline_deep_models/baseline.py", line 184, in <module>
+            ## write_to_log(glove_dim=glove_dim, model_type=model_type, model_args=model_to_args.get(model_type),
+            ## NameError: name 'model_to_args' is not defined
+            write_to_log(glove_dim=glove_dim, model_type=model_type, model_args=model_to_args.get(model_type),
                          log_path=args.log_path, log_filename=args.log_filename,
                          data_type=preprocessed_data_type, mean_accuracy=mean_accuracy, std_accuracy=std_accuracy)
 
