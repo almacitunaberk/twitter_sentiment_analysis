7009e055d9df96be8e649d5e7ab13eb04481a943
Ubuntu <ubuntu@ip-172-31-40-189.eu-west-2.compute.internal>
2024-07-16 12:44:28 +0000

    Revert "AWS runnable"
    This reverts commit 9c50826ee546f8e4ea877bf638d45c38cd1e2f1b.

--- src/baseline_deep_models/baseline.py
+++ src/baseline_deep_models/baseline.py
@@ -21,22 +21,18 @@ from collections import Counter
 from torch.utils.data import TensorDataset, DataLoader
 import time
 from tqdm import tqdm
-from sklearn.model_selection import train_test_split
-from torchtext import datasets
-from torchtext import data
-import torchtext
 
 if torch.cuda.is_available():
     print("GPU is available")
 else:
     print("GPU is NOT available")
 
-#device = torch.device("gpu") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("gpu") if torch.cuda.is_available() else torch.device("cpu")
 
 class BiLSTM(nn.Module):
     def __init__(self, embed_dim, drop_prob):
         super().__init__()
-        self.hidden_size = 50
+        self.hidden_size = 100
         self.input_size = embed_dim
         self.num_layers = 1
         self.bidirectional = True
@@ -55,22 +51,6 @@ class BiLSTM(nn.Module):
         output = self.fc(x)
         pred = torch.sigmoid(output[-1])
         return pred
-
-class CustomDataset(torchtext.data.Dataset):
-    def __init__(self, data, TEXT, LABEL):
-        fields = [("tweet", TEXT), ("label", LABEL), (None, None)]
-        dataset = []
-        for i in range(data.shape[0]):
-            tweet = data.text[i]
-            label = data.labels[i]
-            dataset.append(data.Example.fromlist([text, label], fields))
-
-    @classmethod
-    def splits(cls, text_field, label_field, root='data',
-               train='train', test='test', **kwargs):
-        return super().splits(
-            root, text_field=text_field, label_field=label_field,
-            train=train, validation=None, test=test, **kwargs)
 
 
 def write_to_log(model_type:str,
@@ -99,12 +79,17 @@ def write_to_log(model_type:str,
 def load_glove_embeddings(glove_path: str):
     print("Loading GloVe embeddings")
     embeddings = {}
+    total = 0
     with open(glove_path, "r") as f:
-        for line in f:
-            word, weights = line.split(maxsplit=1)
-            weights = np.fromstring(weights, "f", sep=" ")
-            embeddings[word] = weights
-    print("Loaded GloVe embeddings")
+        total = len(f.readlines())
+
+    with open(glove_path, "r") as f:
+        with tqdm(total=total) as pbar:
+            for line in f:
+                word, weights = line.split(maxsplit=1)
+                weights = np.fromstring(weights, "f", sep=" ")
+                embeddings[word] = weights
+                pbar.update(1)
     return embeddings
 
 def tweet_embed(words, embeddings, glove_dim):
@@ -122,18 +107,7 @@ def tweet_embed(words, embeddings, glove_dim):
         words[i] = mean
     return np.array(words)
 
-def binary_accuracy(preds, y):
-    """
-    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-    """
-
-    #round predictions to the closest integer
-    rounded_preds = torch.round(torch.sigmoid(preds))
-    correct = (rounded_preds == y).float() #convert into float for division
-    acc = correct.sum()/len(correct)
-    return acc
-
-def cross_validation(df, model_type:str, glove_dim:int, glove_path:str):
+def cross_validation(data, model_type:str, glove_dim:int, glove_path:str):
 
     custom_tokenizer = Tokenizer(reduce_len=True, segment_hashtags=True, post_process=True)
 
@@ -141,32 +115,10 @@ def cross_validation(df, model_type:str, glove_dim:int, glove_path:str):
 
     print("Transforming tweets")
 
-    df["text"] = df["text"].apply(lambda x: custom_tokenizer.tokenize_tweet(x))
-    #data[data["text"].map(len) >= 1]
-    df["text"] = df["text"].apply(lambda x: tweet_embed(x, embeddings, glove_dim))
+    data["text"] = data["text"].apply(lambda x: custom_tokenizer.tokenize_tweet(x))
+    data[data["text"].map(len) >= 1]
+    data["text"] = data["text"].apply(lambda x: tweet_embed(x, embeddings, glove_dim))
 
-    train, valid = train_test_split(df, test_size=0.2, random_state=42)
-
-    text_field = data.Field()
-    label_field = data.LabelField(dtype=torch.float)
-    fields = [('text',text_field),('label',label_field),(None, None)]
-
-    train = CustomDataset(train,text_field,label_field)
-    valid = CustomDataset(valid,text_field,label_field)
-
-    print("Number of training samples: {}".format(len(train)))
-    print("Number of validation samples: {}".format(len(valid)))
-
-    BATCH_SIZE = 50
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    #train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits((train_data, valid_data, test), batch_size=BATCH_SIZE,device=device)
-    train_iterator = data.BucketIterator(train_data , batch_size=BATCH_SIZE,device=device)
-    valid_iterator = data.BucketIterator(valid_data, batch_size=BATCH_SIZE,device=device)
-    test_iterator = data.BucketIterator(test, batch_size=BATCH_SIZE,device=device)
-
-    print("Length of train iterator: {}".format(len(train_iterator)))
 
     skf = StratifiedKFold(n_splits=2, shuffle=True, random_state=42)
     aggregated_acc = []
@@ -174,7 +126,6 @@ def cross_validation(df, model_type:str, glove_dim:int, glove_path:str):
     tweets = data["text"].values
     labels = data["labels"].values
 
-    """
     aggregated_acc = []
 
     for i, (train_indices, test_indices) in enumerate(skf.split(tweets, labels)):
@@ -184,31 +135,8 @@ def cross_validation(df, model_type:str, glove_dim:int, glove_path:str):
         val_tweets = tweets[test_indices]
         val_labels = labels[test_indices]
 
-        non_zero_indices = []
-
-        for i, tweet in enumerate(training_tweets):
-            if len(tweet) != 0:
-                non_zero_indices.append(i)
-
-        training_tweets = np.asarray(training_tweets[non_zero_indices])
-        training_labels = np.asarray(training_labels[non_zero_indices])
-
-        print("Length of training tweets {}".format(training_tweets.size))
-
-        non_zero_indices = []
-
-        for i, tweet in enumerate(val_tweets):
-            if len(tweet) != 0:
-                non_zero_indices.append(i)
-
-        val_tweets = np.asarray(val_tweets[non_zero_indices])
-        val_labels = np.asarray(val_labels[non_zero_indices])
-
-        print("Length of validation tweets {}".format(val_tweets.size))
-
         if model_type == "bilstm":
             bilstm = BiLSTM(glove_dim, 0.2)
-            bilstm.cuda()
             loss_func = nn.BCELoss()
             optimizer = torch.optim.Adam(bilstm.parameters(), lr=0.001)
             scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1)
@@ -216,7 +144,7 @@ def cross_validation(df, model_type:str, glove_dim:int, glove_path:str):
             val_losses = []
             train_accs = []
             val_accs = []
-            for epoch in range(2):
+            for epoch in range(4):
                 epoch_start_time = time.time()
                 print("Epoch: {}".format(epoch+1))
                 train_loss = 0
@@ -295,43 +223,6 @@ def cross_validation(df, model_type:str, glove_dim:int, glove_path:str):
             mean_accuracy = np.array(aggregated_acc).mean()
             std_accuracy = np.array(aggregated_acc).std()
             return mean_accuracy, std_accuracy
-    """
-    if model_type == "bilstm":
-        epoch_loss = 0
-        bilstm = BiLSTM(glove_dim, 0.2)
-        bilstm.cuda()
-        loss_func = nn.BCELoss()
-        optimizer = torch.optim.Adam(bilstm.parameters(), lr=0.001)
-        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1)
-        counter=1
-        acc=[]
-        for epoch in range(2):
-            bilstm.train()
-            for batch in train_iterator:
-                optimizer.zero_grad()
-                predictions = bilstm(batch.text).squeeze(1)
-                loss = loss_func(predictions, batch.label)
-                #rl.append(loss)
-                loss.backward()
-                optimizer.step()
-                epoch_loss += loss.item()
-                counter=counter+1
-
-                if(counter%2010==0):
-                    print('Epoch: [{}/{}] | Step: [{}/{}] | Loss: {} |'.format(epochs+1, num_epochs, int((counter)/2010),10 , round(epoch_loss/2010,3)))
-                    #counter=0
-                    epoch_loss=0
-            epoch_acc = 0
-            bilstm.eval()
-            for batch in valid_iterator:
-                predictions = bilstm(batch.text).squeeze(1)
-                accuracy = binary_accuracy(predictions, batch.label)
-                epoch_acc += accuracy.item()
-            acc.append(valid_acc)
-            print(f'| Epoch: {epochs+1:02} | Val. Acc: {valid_acc*100:.2f}% | ---%Saving the model%---')
-
-
-    #return epoch_loss / len(iterator)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -349,7 +240,6 @@ if __name__ == "__main__":
     train_df = pd.read_csv(args.input_path)
     train_df = train_df.sample(n=100000) # TODO: Comment this line when not testing
     train_df = train_df.dropna()
-    print("Started with data length: {}".format(train_df.shape[0]))
     """
     pos_tweets = np.array(train_df[train_df["labels"] == 1]["text"].values)[:1000]
     pos_labels = labels[:1000]
@@ -379,5 +269,5 @@ if __name__ == "__main__":
                                                             glove_path=args.glove_path)
             write_to_log(glove_dim=glove_dim, model_type=model_type,
                          log_path=args.log_path, log_filename=args.log_filename,
-                         data_type="processed", mean_accuracy=mean_accuracy, std_accuracy=std_accuracy)
+                         data_type=preprocessed_data_type, mean_accuracy=mean_accuracy, std_accuracy=std_accuracy)
 
